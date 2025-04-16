7ef0b1864d5af7917b0fd6cd08421a286c976c4b
Mohammad Amin Seifinia <53443483+thewitcher745@users.noreply.github.com>
2025-04-17 00:54:55 +0330

    Add files via upload

---
+++ redirect.js
@@ -0,0 +1,27 @@
+window.onload = function () {
+  // Define the dictionary of key-value pairs (link IDs to target URLs)
+  const linkDictionary = {
+    CAN_Algo: "https://t.me/+6V6xxWEXw25kOTk0",
+    Cryptoboard: "https://t.me/+dmQd0WWdqCc1NjBk",
+  };
+
+  // Get the start_param from URL parameters
+  const urlParams = new URLSearchParams(window.Telegram.WebApp.initData);
+  const startParam = urlParams.get("start_param");
+
+  // Navigate to the corresponding link if it exists in the dictionary
+  if (startParam && linkDictionary[startParam]) {
+    const link = linkDictionary[startParam];
+
+    const channelLinkElement = document.createElement("a");
+    channelLinkElement.href = link;
+    channelLinkElement.innerText = "Open Channel";
+
+    channelLinkElement.click();
+
+    window.Telegram.WebApp.close();
+  } else {
+    // Handle case when start_param is not found in the dictionary
+    window.Telegram.WebApp.close();
+  }
+};
